08dff0ab0500ae0f2c7b94ef4c11e18b5c231721
Cameron Cooks <cameronc56@ymail.com>
2015-04-21 15:58:30 -0700

    threadwidget tweaks

--- static/js/threadWidget.js
+++ static/js/threadWidget.js
@@ -1,19 +1,34 @@
 var threadWidget = {};
 
 threadWidget.view = function(ctrl, args) {
-    return m("div", [
-        m(".media", {style: "border-style: solid; border-width: 3px; margin-top: 10px; border-color: #8A9BB8; border-radius: 5px;"}, [
-            m(".media-left.media-top.pull-left", [
-                m("a", {href: "", config: m.route}, [
-                    m("img.media-object", {src: "https://i.imgur.com/eevjESG.jpg", alt: "Quack!~", style: "height: 64px; width: 64px;"})
-                ])
+    return m(".container-fluid", [
+        m(".row", [
+            m("a", {href: "/forum/page/1#upvote"}, [
+
             ]),
-            m(".media-body.media-right", [
-                m("a", {href: "", config: m.route}, [
-                    m("h4.media-heading", args.title)
+            m("a", {href: "/forum/page/1#downvote", config: m.route})
+            m("span.glyphicon.glyphicon-arrow-up.pull-left", {style: "margin-top: 35px;"}),
+            m("span.glyphicon.glyphicon-arrow-down.pull-left", {style: "margin-top: 35px; margin-right: 10px;"}),
+            m(".media",
+            {
+                style: "margin-top: 10px; " +
+                "border-style: solid; " +
+                "border-width: 2px; " +
+                "border-color: #8A9BB8; " +
+                "border-radius: 4px;"
+            }, [
+                m(".media-left", [
+                    m("a", {href: "", config: m.route}, [
+                        m("img.media-object", {src: "https://i.imgur.com/eevjESG.jpg", alt: "Quack!~", style: "height: 64px; width: 64px;"})
+                    ])
                 ]),
-                //m("p", "upvote downvote"),
-                m("p", "Submitted 3 hours ago by Cameronc")
+                m(".media-body", [
+                    m("a", {href: "", config: m.route}, [
+                        m("h4.media-heading.clearfix", args.title)
+                    ]),
+
+                    m("p.pull-left", "Submitted 3 hours ago by Cameronc")
+                ])
             ])
         ])
     ])
